3f623c900679664038bc56704e4c6bbe49572c3d
masonp <masonp@seattleu.edu>
2017-11-29 15:35:59 -0500

    card test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 //Start of beer cards//
+class MaterialCard {
+  constructor (){
+    $(function() {
+        $('.material-card > .mc-btn-action').click(function () {
+            let card = $(this).parent('.material-card');
+            let icon = $(this).children('i');
+            icon.addClass('fa-spin-fast');
+            if (card.hasClass('mc-active')) {
+                card.removeClass('mc-active');
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-arrow-left')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-bars');
+                }, 800);
+            } else {
+                card.addClass('mc-active');
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-bars')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-arrow-left');
+                }, 800);
+            }
+        });
+    });
+  }
+}
+ document.addEventListener('DOMContentLoaded', function(){}
 
 (function($) {
   materialCard.getElementById("card");
@@ -11,7 +40,7 @@
     this.icon = $(element).children('.mc-btn-action').children('i');
     this.card_activator = options.card_activator;
     this.timing = this.getTransitionTiming();
-
+  
 
     let that = this;
 
@@ -35,10 +64,10 @@
 
       });
     }
-
+  
   };
   
-  document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('click', function(event){
 
   cardFunctions = {
     icon_close: 'fa-arrow-left',
